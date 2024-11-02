1e3605157d5877553e3fbec73ecea4d446e8e4a5
evignacio <reignomo27@hotmail.com>
2024-11-02 17:08:32 -0300

    Criptografando senha do cliente

--- gestaocontas/src/main/java/org/techchallenge/gestaocontas/application/service/CadastroClienteService.java
+++ gestaocontas/src/main/java/org/techchallenge/gestaocontas/application/service/CadastroClienteService.java
@@ -4,14 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.techchallenge.gestaocontas.domain.entity.Cliente;
 import org.techchallenge.gestaocontas.domain.repository.ClienteRepository;
+import org.techchallenge.gestaocontas.domain.service.GerarSenhaService;
 
 @Service
 @RequiredArgsConstructor
 public class CadastroClienteService {
 
     private final ClienteRepository clienteRepository;
+    private final GerarSenhaService gerarSenhaService;
 
     public void cadastrar(Cliente cliente) {
+        cliente.setSenhaAcesso(this.gerarSenhaService.criptografarSenha(cliente.getSenhaAcesso()));
         this.clienteRepository.salvar(cliente);
     }
 }
