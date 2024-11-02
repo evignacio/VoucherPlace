704efa8d52c4a4e491dc2a201b461e18614cbc66
evignacio <reignomo27@hotmail.com>
2024-11-02 15:04:05 -0300

    Implementando tratamento de erros @Bean validations

--- web/src/main/java/org/techchallenge/resource/v1/handler/ControllerHandlerException.java
+++ web/src/main/java/org/techchallenge/resource/v1/handler/ControllerHandlerException.java
@@ -2,13 +2,41 @@ package org.techchallenge.resource.v1.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.techchallenge.gestaocontas.domain.exception.ApplicationException;
+import org.techchallenge.gestaocontas.domain.exception.ExceptionType;
 import org.techchallenge.resource.v1.response.Resposta;
+
+import java.util.Iterator;
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerHandlerException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Resposta> handle(MethodArgumentNotValidException ex) {
+        var mensagemDeErro = new StringBuilder();
+        Iterator<FieldError> fieldErrors = ex.getFieldErrors().iterator();
+
+        while (fieldErrors.hasNext()) {
+            FieldError field = fieldErrors.next();
+            mensagemDeErro.append(field.getField())
+                    .append(": ")
+                    .append(field.getDefaultMessage());
+
+            if (fieldErrors.hasNext())
+                mensagemDeErro.append(", ");
+        }
+
+        return new ResponseEntity<>(
+                Resposta.criar(ExceptionType.VALIDATION.getCodigo(), mensagemDeErro.toString()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<Resposta> handleApplicationException(ApplicationException exception) {
--- web/src/main/java/org/techchallenge/resource/v1/request/CadastroEmpresaRequest.java
+++ web/src/main/java/org/techchallenge/resource/v1/request/CadastroEmpresaRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CadastroEmpresaRequest(
-        @Size(min = 14, max = 14, message = "deve ter no 14 caracteres") String cnpj,
+        @Size(min = 14, max = 14, message = "deve ter 14 caracteres") String cnpj,
         @Size(min = 5, max = 36, message = "deve ter entre 5 e 36 caracteres") String emailAcesso,
         @Valid @NotNull Telefone telefone,
         @Size(min = 5, max = 36, message = "deve ter entre 5 e 36 caracteres") String emailContato) {
